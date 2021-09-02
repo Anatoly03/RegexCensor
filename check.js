2d58e21a542602accc654447de5e961b9b6c5f30
Anatoly Wein <anatoly.wein@gmail.com>
2021-09-02 20:11:26 +0200

    add: Filter('*') add: transform

--- check.js
+++ check.js
@@ -6,18 +6,16 @@ const rl = readline.createInterface({
     output: process.stdout,
 })
 
-const filter = new Filter({
-    fields: ['*'],
-})
+const filter = new Filter('*')
 
 async function read_line() {
     // https://nodejs.org/en/knowledge/command-line/how-to-prompt-for-command-line-input/
     rl.question('', function (str) {
         if (filter.check(str)) {
             // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-            console.log('> \x1b[31mTRIGGERED\x1b[0m')
+            console.log('\x1b[31mTRIGGERED\x1b[0m')
         } else {
-            console.log('> \x1b[32mSAFE\x1b[0m')
+            console.log('\x1b[32mSAFE\x1b[0m')
         }
         read_line()
     })
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { Filter } from './lib/filter.js'
 
 const str = 'Hello World!'
-const filter = new Filter({
-    fields: ['sexual'],
-})
+const filter = new Filter('*')
 
 console.log(filter.check(str))
+
+console.log('ằss'.normalize("NFD").replace(/[\u0300-\u036f]/g, ""));
