6cb94a95f64f790de693fca2bf6b27d5e1296845
Anatoly Wein <anatoly.wein@gmail.com>
2021-09-02 18:10:04 +0200

    add: patterns

---
+++ config/patterns.js
@@ -0,0 +1,14 @@
+export default 
+[
+    {
+        name: "sexual",
+        patterns: [
+            /\babortion\b/i,
+            /\banal\w*/i,
+            /\bpiss\w*/i,
+            /\b\w*dick\w*\b/i,
+            /\bass\w*/i,
+            /\bsmartass\b\w*/i,
+        ]
+    }
+]
