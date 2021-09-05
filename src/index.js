8963d4ca9f0b8b255c05722b25d3d16cb5e06799
Anatoly Wein <anatoly.wein@gmail.com>
2021-09-05 11:53:18 +0200

    move check.js to index with debug env

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import readline from 'readline'
 import Filter from './filter.js'
-import { extend_lat } from './config/latinize-extend.js'
+import { extend_lat, lat } from './config/latinize-extend.js'
 extend_lat()
 
 import './methods/check.js'
@@ -9,4 +10,32 @@ import './methods/replace.js'
 
 export default function (config) {
     return new Filter(config)
+}
+
+if (process.env.DEBUG_FILTER) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+
+    const filter = new Filter('*')
+
+    async function read_line() {
+        // https://nodejs.org/en/knowledge/command-line/how-to-prompt-for-command-line-input/
+        rl.question('', function (str) {
+            console.log('   LATINZED: \x1b[36m', lat(str), '\x1b[0m')
+            console.log('   REPLACED: \x1b[36m', filter.replace(str), '\x1b[0m')
+            if (filter.check(str)) {
+                for (const pat of filter.matchString(str)) {
+                    console.log('  ', pat)
+                }
+            } else {
+                // https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+                console.log('   \x1b[32msafe\x1b[0m')
+            }
+            read_line()
+        })
+    }
+
+    read_line()
 }
