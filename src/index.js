78fccd2c90e7e60909e651a25a9b56c27a829dbc
Anatoly Wein <anatoly.wein@gmail.com>
2021-09-09 10:55:48 +0200

    cleanup: filter.add method

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Filter from './filter.js'
 import { extend_lat, lat } from './config/latinize-extend.js'
 extend_lat()
 
+import './methods/add.js'
 import './methods/check.js'
 import './methods/find.js'
 import './methods/match.js'
---
+++ src/methods/add.js
@@ -0,0 +1,38 @@
+import patterns from '../config/patterns.js'
+import Filter from '../filter.js'
+
+/**
+ * @method addField
+ * @param {string} key Field Name to add
+ */
+Filter.prototype.addField = function (key) {
+    if (typeof key != 'string')
+        throw 'Filter.addField(key) ~ key is expected to be of type string'
+
+    for (const field of patterns) {
+        if (field.name == key) {
+            this.add(field.patterns)
+            return
+        }
+    }
+}
+
+/**
+ * @method add
+ * @param {RegExp | RegExp[]} pat Field Name to add
+ */
+Filter.prototype.add = function (pat) {
+    if (pat instanceof RegExp) {
+        for (const pat of this.patterns) {
+            if (pat.test(str)) return true
+        }
+        return
+    } else if (Array.isArray(pat)) {
+        for (const field of pat) {
+            this.patterns.push(field)
+        }
+        return
+    }
+    throw 'Filter.add() expects RegExp or an array of regex patterns as parameter input.'
+}
+
