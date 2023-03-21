27df94638cbd4e3483ce4f4f6ac1a2edf9493e90
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-03-21 17:10:54 +0100

    add replace

---
+++ lib/src/replace.rs
@@ -0,0 +1,12 @@
+use fancy_regex::Regex;
+use wasm_bindgen::prelude::*;
+use super::filter::Filter;
+
+#[wasm_bindgen]
+impl Filter {
+    pub fn replace(&mut self, content: String) -> String {
+        todo!();
+
+        false
+    }
+}
