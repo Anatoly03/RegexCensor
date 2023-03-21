d75a1f861d4d2af6b6a14c21afc8fbf00900b4e0
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-03-21 17:48:51 +0100

    impl replace

--- lib/src/replace.rs
+++ lib/src/replace.rs
@@ -4,7 +4,21 @@ use super::filter::Filter;
 
 #[wasm_bindgen]
 impl Filter {
-    pub fn replace(&mut self, content: String) -> String {
-        todo!()
+    pub fn replace(&mut self, mut content: String) -> String {
+        let mut ranges = Vec::new();
+
+        for pat in self.patterns_read() {
+            for mtch in pat.find_iter(&content) {
+                if let Ok(m) = mtch {
+                    ranges.push(m.start() .. m.end())
+                }
+            }
+        }
+
+        for range in ranges {
+            content.replace_range(range, "*");
+        }
+
+        content
     }
 }
