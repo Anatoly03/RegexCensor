553f11491d53f6b7c86cd03c38771e1437aa208c
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-03-21 17:39:47 +0100

    impl find patterns

--- lib/src/find.rs
+++ lib/src/find.rs
@@ -26,7 +26,19 @@ impl Filter {
 
 
     pub fn find_patterns(&mut self, content: String) -> JsValue {
-        todo!();
-        // TODO array of matched patterns
+        let mut patterns = Vec::new();
+
+        for pat in self.patterns_read() {
+            if let Ok(true) = pat.is_match(&content) {
+                patterns.push(pat.to_string());
+            }
+        }
+        
+        JsValue::from(
+            patterns.clone()
+                .into_iter()
+                .map(|x| JsValue::from_str(x.as_str()))
+                .collect::<Array>(),
+        )
     }
 }
