173286767e47043f15bf73550bdaa95e4d2c6516
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-03-21 17:06:41 +0100

    add find

---
+++ lib/src/find.rs
@@ -0,0 +1,14 @@
+use js_sys::Array;
+use fancy_regex::Regex;
+use wasm_bindgen::prelude::*;
+use super::filter::Filter;
+
+#[wasm_bindgen]
+impl Filter {
+    pub fn find(&mut self, rgx: String) -> JsValue {
+        todo!();
+        // TODO array of matched words
+
+        false
+    }
+}
