e424782e5188a930aac20d798e5507b59995d9fc
Anatoly Weinstein <anatoly.wein@gmail.com>
2023-03-23 17:07:39 +0100

    add all presets

--- src/preset.js
+++ src/preset.js
@@ -2,6 +2,8 @@ import { Filter } from "../build/lib.js";
 import fs from "fs";
 
 Filter.prototype.add_preset = function (preset) {
+    if (preset == '*') this.add_all_presets()
+
     const file = fs.readFileSync("../PATTERNS.md", "utf-8"); // node_modules/regexcensor/PATTERNS.md
     const begin = file.indexOf("## PATTERNS");
     const lines = file.substring(begin).split("\n");
@@ -12,8 +14,26 @@ Filter.prototype.add_preset = function (preset) {
         if (line.startsWith("| `" + preset)) {
             for (; lines[i + 1].startsWith("|| `"); i++) {
                 const end_expr = lines[i + 1].indexOf('` |')
-                const expr = lines[i + 1].substring(4, end_expr)
+                const expr = lines[i + 1].substring(4, end_expr).replace('\|', '|')
+                this.add(expr)
             }
+            break
+        }
+    }
+};
+
+Filter.prototype.add_all_presets = function () {
+    const file = fs.readFileSync("../PATTERNS.md", "utf-8"); // node_modules/regexcensor/PATTERNS.md
+    const begin = file.indexOf("## PATTERNS");
+    const lines = file.substring(begin).split("\n");
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+
+        if (line.startsWith("|| `" + preset)) {
+            const end_expr = lines[i + 1].indexOf('` |')
+            const expr = lines[i + 1].substring(3, end_expr).replace('\|', '|')
+            this.add(expr)
         }
     }
 };
